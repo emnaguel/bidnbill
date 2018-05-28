aaf6e54e3552246c226181701eb406a774217530
Ayoub Karioun <kariounayoub@gmail.com>
2018-05-28 19:09:10 +0200

    create a bill / create an auction

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,4 +1,5 @@
 class Bill < ApplicationRecord
   belongs_to :client, class_name: 'User', foreign_key: 'user_id'
-  has_one :auction
+  has_one :auction, dependent: :destroy
+  CATEGORIES = ['Electricité', 'Gaz', 'Téléphonie', 'Box Internet']
 end
