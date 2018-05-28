4139e77ba674b92ffe9615391c1af7d45a9e0ba7
Emna Guellaty <eguellaty@gmail.com>
2018-05-28 17:15:16 +0000

    bid

--- app/models/bid.rb
+++ app/models/bid.rb
@@ -1,4 +1,6 @@
 class Bid < ApplicationRecord
   belongs_to :auction
-  belongs_to :user
+  belongs_to :provider, class_name: "User", foreign_key: "user_id"
+  has_one :bill, throught: :auction
+  has_one :client, throught: :auction
 end
