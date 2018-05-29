c88a8cb65b95c56c49298bd742a15b08dd0e0a39
Ayoub Karioun <kariounayoub@gmail.com>
2018-05-29 17:14:46 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,32 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
+provider = User.create(user_type: "provider", email: "provider@gmail.com", password: "azerty", first_name: "My", last_name: "Provider")
+client = User.create(user_type: "client", email: "client@gmail.com", password: "azerty", first_name: "My", last_name: "Client")
+provider2 = User.create(user_type: "provider2", email: "provider2@gmail.com", password: "azerty", first_name: "My", last_name: "Provider2")
 
-users = User.create(user_type: "provider", email: "provider@gmail.com", password: "azerty", first_name: "Jerome", last_name: "Dubois")
-users = User.create(user_type: "client", email: "client@gmail.com", password: "azerty", first_name: "Heba", last_name: "Dubois")
-users = User.create(user_type: "client", email: "another_client@gmail.com", password: "azerty", first_name: "Julie", last_name: "Dupont")
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-bills = Bill.create(user_id: 2, category: "Gaz", address: "Paris")
-bills = Bill.create(user_id: 2, category: "Electricité", address: "Marseille")
-bills = Bill.create(user_id: 3, category: "Téléphonie", address: "Deauville")
+Bill.create(user_id: 2, category: "Electricité", address: "Paris")
+Bill.create(user_id: 2, category: "Gaz", address: "Marseille")
+Bill.create(user_id: 2, category: "Téléphonie", address: "Deauville")
+Bill.create(user_id: 2, category: "Box Internet", address: "Paris")
 
-bills = Bill.create(user_id: 1, category: "Gaz", address: "Deauville")
+Auction.create(bill_id: 1)
+Auction.create(bill_id: 2)
+Auction.create(bill_id: 3)
+Auction.create(bill_id: 4)
 
 
-
-
-auctions = Auction.create(bill_id: 2)
-auctions = Auction.create(bill_id: 2)
-auctions = Auction.create(bill_id: 3)
-
-auctions = Auction.create(bill_id: 4)
-
-
-
-
-bids = Bid.create(auction_id: 1, user_id: 2, status: "completed", payment_status: "pending")
-bids = Bid.create(auction_id: 2, user_id: 2, status: "pending", payment_status: "pending")
-bids = Bid.create(auction_id: 4, user_id: 1, status: "completed", payment_status: "cancelled")
-
+Bid.create(auction_id: 1, user_id: 3, status: "pending", payment_status: "pending")
+Bid.create(auction_id: 2, user_id: 1, status: "pending", payment_status: "pending")
+Bid.create(auction_id: 3, user_id: 1, status: "completed", payment_status: "pending")
+Bid.create(auction_id: 4, user_id: 1, status: "completed", payment_status: "completed")
