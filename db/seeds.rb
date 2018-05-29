23363043af71f9b8acbbe0c9d5e90cf6503bc0d6
Ayoub Karioun <kariounayoub@gmail.com>
2018-05-29 19:24:53 +0200

    SQL logic in provider_dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,22 @@
+Bill.destroy_all
+User.destroy_all
+
 provider = User.create(user_type: "provider", email: "provider@gmail.com", password: "azerty", first_name: "My", last_name: "Provider")
 client = User.create(user_type: "client", email: "client@gmail.com", password: "azerty", first_name: "My", last_name: "Client")
 provider2 = User.create(user_type: "provider2", email: "provider2@gmail.com", password: "azerty", first_name: "My", last_name: "Provider2")
 
-Bill.create(user_id: 2, category: "Electricité", address: "Paris")
-Bill.create(user_id: 2, category: "Gaz", address: "Marseille")
-Bill.create(user_id: 2, category: "Téléphonie", address: "Deauville")
-Bill.create(user_id: 2, category: "Box Internet", address: "Paris")
+bill_elec = Bill.create(client: client, category: "Electricité", address: "Paris")
+bill_gaz = Bill.create(client: client, category: "Gaz", address: "Marseille")
+bill_tel = Bill.create(client: client, category: "Téléphonie", address: "Deauville")
+bill_box = Bill.create(client: client, category: "Box Internet", address: "Paris")
 
-Auction.create(bill_id: 1)
-Auction.create(bill_id: 2)
-Auction.create(bill_id: 3)
-Auction.create(bill_id: 4)
+a1 = Auction.create(bill: bill_elec)
+a2 = Auction.create(bill: bill_gaz)
+a3 = Auction.create(bill: bill_tel)
+a4 = Auction.create(bill: bill_box)
 
 
-Bid.create(auction_id: 1, user_id: 3, status: "pending", payment_status: "pending")
-Bid.create(auction_id: 2, user_id: 1, status: "pending", payment_status: "pending")
-Bid.create(auction_id: 3, user_id: 1, status: "completed", payment_status: "pending")
-Bid.create(auction_id: 4, user_id: 1, status: "completed", payment_status: "completed")
+Bid.create(auction: a1, provider: provider2, status: "pending", payment_status: "pending")
+Bid.create(auction: a2, provider: provider, status: "pending", payment_status: "pending")
+Bid.create(auction: a3, provider: provider, status: "completed", payment_status: "pending")
+Bid.create(auction: a4, provider: provider, status: "completed", payment_status: "completed")
