551e1ed6dd358eb62924b5dad5a7f8e179cef532
Emna Guellaty <eguellaty@gmail.com>
2018-06-05 20:13:14 +0000

    grid

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,5 +1,21 @@
 import GMaps from 'gmaps/gmaps.js';
 
+
+const othermapElement = document.getElementById('othermap');
+if (othermapElement) { // don't try to build a map if there's no div#map to inject in
+  const map = new GMaps({ el: '#othermap', lat: 0, lng: 0 });
+  const markers = JSON.parse(othermapElement.dataset.markers);
+  map.addMarkers(markers);
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(14);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+}
+
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
