cbe984abe0ec84cd69a2a82e2a42e63fce32e8e7
Ayoub Karioun <kariounayoub@gmail.com>
2018-06-07 18:18:42 +0200

    migration city zip

--- app/javascript/components/autocomplete.js
+++ app/javascript/components/autocomplete.js
@@ -1,9 +1,52 @@
+function onPlaceChanged() {
+  var place = this.getPlace();
+  var components = getAddressComponents(place);
+
+  document.getElementById('bill_zip_code').value = components.zip_code;
+  document.getElementById('bill_city').value = components.city;
+}
+
+function getAddressComponents(place) {
+
+  if (window.console && typeof console.log === "function") {
+    console.log(place);
+  }
+  var zip_code = null;
+  var city = null;
+  for (var i in place.address_components) {
+    var component = place.address_components[i];
+    for (var j in component.types) {
+      var type = component.types[j];
+      if (type === 'street_number') {
+        street_number = component.long_name;
+      } else if (type === 'route') {
+        route = component.long_name;
+      } else if (type === 'postal_code') {
+        zip_code = component.long_name;
+      } else if (type === 'locality') {
+        city = component.long_name;
+      } else if (type === 'postal_town' && city === null) {
+        city = component.long_name;
+      } else if (type === 'country') {
+        country_code = component.short_name;
+      }
+    }
+  }
+  console.log(city);
+  console.log(zip_code);
+  return {
+    zip_code: zip_code,
+    city: city,
+  };
+}
+
 function autocomplete() {
   document.addEventListener("DOMContentLoaded", function() {
     var billAddress = document.getElementById('bill_address');
 
     if (billAddress) {
       var autocomplete = new google.maps.places.Autocomplete(billAddress, { types: [ 'geocode' ] });
+      google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
       google.maps.event.addDomListener(billAddress, 'keydown', function(e) {
         if (e.key === "Enter") {
           e.preventDefault(); // Do not submit the form on Enter.
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_06_171319) do
+ActiveRecord::Schema.define(version: 2018_06_07_160907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 2018_06_06_171319) do
     t.float "latitude"
     t.float "longitude"
     t.integer "consumption"
+    t.string "city"
+    t.string "zip_code"
     t.index ["category_id"], name: "index_bills_on_category_id"
     t.index ["user_id"], name: "index_bills_on_user_id"
   end
